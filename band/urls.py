1a2974c4c9cddf553fdebf824b0d2d59e29d535c
Raj Vansia <vansia112@gmail.com>
2015-10-13 02:04:12 +0000

    resjJson

--- band/urls.py
+++ band/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from band import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'music.views.home', name='home'),
@@ -9,7 +10,8 @@ urlpatterns = patterns('',
 
       url(r'^$', 'band.views.all_bands'),
       url(r'^(?P<band_id>\d+)/$', 'band.views.band'),
-      url(r'^api/(?P<pk>\d+)/$', views.BandDetail.as_view()),
+      url(r'^api/(?P<pk>[0-9]+)/$', views.BandDetail.as_view()),
+      
 )
 
-urlpatterns=format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
--- band/views.py
+++ band/views.py
@@ -5,12 +5,12 @@ from serializers import BandSerializer
 #interger argument
 def all_bands(request):
     return render(request,"band/all_bands.html")
-
+# class base views a serilize that data
 class BandDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Band.objects.all()
     serializer_class=BandSerializer
 
-def band(request, band_id):
+def band(request, band_id,format=None):
     band=Band.objects.get(pk=band_id)
     return render(request, "band/band.html",{'band':band})
 #django dictionay key value pairs
