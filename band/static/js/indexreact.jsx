d88ebe77b6bfb57bd88da2f0dcb2fb01203b0bf6
Raj Vansia <vansia112@gmail.com>
2015-10-13 13:55:31 +0000

    newreactrenders

--- band/static/js/indexreact.jsx
+++ band/static/js/indexreact.jsx
@@ -1,4 +1,10 @@
-
+var bands=[
+    {name:"rag", image:"http://i.imgur.com/tM8UteK.png"},
+     {name:"rag2", image:"http://i.imgur.com/tM7UteK.png"},
+     {name:"rag3", image:"http://i.imgur.com/tM6UteK.png"},
+     {name:"rag4", image:"http://i.imgur.com/tM5UteK.png"}
+    ]
+      
       var ExampleApplication = React.createClass({
         render: function() {
           var elapsed = Math.round(this.props.elapsed  / 100);
@@ -17,3 +23,59 @@
         );
       }, 50);
     
+    
+    var BandComponent=React.createClass({
+        getInitialState: function(){
+            return {customText:""};
+        },
+  customClickFunction:function(){
+    alert("you clicked this nigga")
+    this.setState({customText: 'You clicked it nigga'});
+  },
+    render: function(){
+      var testStyle={fontSize: '18px' ,color:'green'}
+        return(
+          <div style={testStyle}>
+          <h1>{this.state.customText}</h1>
+            <button onClick={this.customClickFunction}>Click Me!!</button>
+            {this.props.bands.map(function(band){
+            return(
+            <div>
+              <h1> name={band.name} </h1>
+              <img src={band.image} />
+              </div>
+              )
+            })}
+          
+          </div>
+            )
+
+    }
+
+});
+React.render(
+<BandComponent  bands={bands}/>,
+document.getElementById('contents')
+
+)
+// var Band=React.createClass({
+//     render: function(){
+//         return(
+//             <div>
+//             <h2>Raj</h2>
+//             <img src="https://facebook.github.io/react/img/logo.svg" />
+//             </div>
+//             )
+//     }
+// });
+
+// React.render(
+// <Band  />,
+// document.getElementById('contentss')
+
+// )
+
+
+
+//everyword caps
+//render always returns somthing need parent child relationhip
